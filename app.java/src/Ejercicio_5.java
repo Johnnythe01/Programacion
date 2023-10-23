14f8db7db3bf9a87a97ab24d7b5813513c3c27d4
John <jbarragandela@nigul.cide.es>
2023-10-23 19:47:46 +0200

    upload

---
+++ app.java/src/Ejercicio_5.java
@@ -0,0 +1,8 @@
+public class Ejercicio_5 {
+    public static void main(String [] args) {
+        int x = 23;
+                System.out.println("Hoy es dia: " + x);
+                
+                
+    }
+}
