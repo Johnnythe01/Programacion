77b18a571f26318c43759437b310196d0e236474
John <jbarragandela@nigul.cide.es>
2023-11-16 19:34:42 +0100

    monkeymonkey

--- MonkeyIsland/practica_MonkeyIsland.java
+++ MonkeyIsland/practica_MonkeyIsland.java
@@ -1,26 +1,32 @@
+import java.util.Scanner;
+
 public class practica_MonkeyIsland {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
+        // Random aleatorio = new Random();
+        // int num = aleatorio.nextInt(5); // de 0 a 4
+        int num = calcularAleatorio(10, 15);
+        System.out.println("El numero aleatorio es: " + num);
 
-    //Random aleatorio = new Random();
-    //int num = aleatorio.nextInt(5); // de 0 a 4
-    int num = calcularAleatorio(10, 15);
-    System.out.println("El numero aleatorio es: " + num);
+        // int num2 = aleatorio.nextInt(100); // de 0 a 99
+        int vidas = 3;
+        int rondas = 0;
+        String[] insultos = {
+                "Has dejado ya de usar pañales?",
+                "No hay palabras para describir lo asqueroso que eres",
+                "He hablado con simios más educados que tu!",
+                "Eres como un dolor en la parte baja de la espalda!" };
+        String[] contestaciones = {
+                "Por qué? Acaso querias pedir uno prestado?",
+                "Si que las hay, sólo que nunca las has aprendido.",
+                "Me alegra que asistieras a tu reunión familiar diaria.",
+                "Ya te están fastidiando otra vez las almorranas, ¿Eh?" };
 
-    //int num2 = aleatorio.nextInt(100); // de 0 a 99
-    int vidas = 3;
-    int rondas = 0;
-    String acierto = "Cabron, Mascachapas, Carasepia, Hecho polvo";
-    String fallo = "Amargao, Reventao, Chapapote, Cuerpo escombro";
-    while (vidas < 0) {
-        if 
     }
+
+    private static int calcularAleatorio(int i, int j) {
+        return 0;
+
     }
-
-private static int calcularAleatorio(int i, int j) {
-    return 0;
-    
-
-}
 }
