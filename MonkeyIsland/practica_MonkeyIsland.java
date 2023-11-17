2ccd1318b1204c5d15f88cb5ae1abffed199ed15
Jonathan <jbarragandela@nigul.cide.es>
2023-11-17 03:46:34 +0100

    practica monosa

--- MonkeyIsland/practica_MonkeyIsland.java
+++ MonkeyIsland/practica_MonkeyIsland.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class practica_MonkeyIsland {
 
@@ -6,27 +7,50 @@ public class practica_MonkeyIsland {
 
         // Random aleatorio = new Random();
         // int num = aleatorio.nextInt(5); // de 0 a 4
-        int num = calcularAleatorio(10, 15);
-        System.out.println("El numero aleatorio es: " + num);
+        // int num = calcularAleatorio(10, 15);
+        // System.out.println("El numero aleatorio es: " + num);
 
         // int num2 = aleatorio.nextInt(100); // de 0 a 99
         int vidas = 3;
         int rondas = 0;
         String[] insultos = {
-                "Has dejado ya de usar pañales?",
-                "No hay palabras para describir lo asqueroso que eres",
+                "0/vacio",
+                "¿Has dejado ya de usar pañales?",
+                "No hay palabras para describir lo asqueroso que eres.",
                 "He hablado con simios más educados que tu!",
-                "Eres como un dolor en la parte baja de la espalda!" };
+                "Eres como un dolor en culo!" };
         String[] contestaciones = {
-                "Por qué? Acaso querias pedir uno prestado?",
+                "0/vacio",
+                "Por qué? Acaso querías pedir uno prestado?",
                 "Si que las hay, sólo que nunca las has aprendido.",
                 "Me alegra que asistieras a tu reunión familiar diaria.",
-                "Ya te están fastidiando otra vez las almorranas, ¿Eh?" };
+                "No te referirás a tus almorranas, ¿Eh?" };
+        System.out.println("¡Ese tío ha insultado a tu madre! no lo podemos permitir, así que... ¡Elije un insulto!");
+            System.out.println("1 " + insultos [1]);
+            System.out.println("2 " + insultos [2]);
+            System.out.println("3 " + insultos [3]);
+            System.out.println("4 " + insultos [4]);
+        Scanner scanner = new Scanner(System.in);
+        int eleccionUsuario = scanner.nextInt();
 
-    }
+        //if (eleccionUsuario >= 1 && eleccionUsuario <=4) {
+            String insultoElegido = insultos[eleccionUsuario];
+            int indiceRespuesta = 1 + (int) (Math.random() * 4);
 
-    private static int calcularAleatorio(int i, int j) {
-        return 0;
+            System.out.println("Tú :" + insultoElegido);
+            System.out.println("El rival: " + contestaciones[indiceRespuesta]);
 
+            if (eleccionUsuario == indiceRespuesta){
+            System.out.println("Oh no, has quedado en ridículo...");
+            
+            } else { System.out.println("¡Este borracho no sabe ni lo que está diciendo! Ganaste el asalto a ese pringao!");
+
+        }
     }
 }
+
+/*
+ * private static int calcularAleatorio(int i, int j) {
+ * return 0;
+ * }
+ */
