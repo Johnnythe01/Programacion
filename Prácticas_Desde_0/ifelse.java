a51a9159102e4c4aaaa28ee28bc38836c08616f5
John <jbarragandela@nigul.cide.es>
2024-01-25 20:38:21 +0100

    commit

---
+++ Prácticas_Desde_0/ifelse.java
@@ -0,0 +1,10 @@
+package Prácticas_Desde_0;
+
+import java.util.Scanner;
+
+public class ifelse{
+    public static void main(String [] Args){
+Scanner scanner = new Scanner (System.in);
+        
+    }
+}
