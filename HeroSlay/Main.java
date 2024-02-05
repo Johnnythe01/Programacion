fc6de942d873107124709aa6e54667bc4cb17534
John <jbarragandela@nigul.cide.es>
2024-02-05 19:11:03 +0100

    HeroSlay

--- HeroSlay/Main.java
+++ HeroSlay/Main.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 
 public class Main {
 public static void main (String[] args) {
+    Personaje Heroe = new Personaje ("Heroe", 10);
+    Personaje Villano = new Personaje ("Villano", 10);
+
     List<Carta> mazo = new ArrayList<>();
     Carta carta1 = new Carta ("Puño", CartaTipo.ATAQUE, 7);
     Carta carta2 = new Carta ("Shock", CartaTipo.STUN, 5);
@@ -13,7 +16,23 @@ public static void main (String[] args) {
     ArrayList<Carta> mazoPersonaje = new ArrayList<>();
     mazoPersonaje.add(carta1);
     mazoPersonaje.add(carta2);
+    mazoPersonaje.add(carta3);
+    Heroe.imprimirInformacion();
+    Villano.imprimirInformacion();
+
+    while (vida > 0) {
+        System.out.println("Héroe! prepara tus cartas, ha aparecido un peligroso villano frente a ti!");
+        
     }
 
-    
+    }   
 }
+
+/*1) Mostraremos la información de ambos jugadores.
+2) Elegiremos aleatoriamente que jugador jugará primero la ronda.
+3) Mostraremos del Mazo del jugador 2 cartas aleatorias.
+4) El jugador en cuestión utilizará una de estas dos cartas.
+5) Se aplicará el efecto de la carta. Ya sea sobre el mismo jugador (si
+es de curación) o sobre el jugador enemigo (si es de ataque).
+6) Si la carta no ha sido un ATURDIR, el siguiente jugador realizará
+los puntos 3, 4 y 5. */
--- HeroSlay/Personaje.java
+++ HeroSlay/Personaje.java
@@ -4,26 +4,29 @@ import java.util.List;
 
 public class Personaje {
     private String nombre;
-    private int vida;
+    private int vida = 10;
     private List<Carta> mazo;
+    private int ronda = 3;
 
-    public Personaje(String nombre, int vida, List<Carta> mazo) {
+    public Personaje(String nombre, int vida) {
         this.nombre = nombre;
         this.vida = vida;
-        this.mazo = mazo;
     }
     public void imprimirInformacion() {
         System.out.println("Nombre del personaje: " + nombre);
         System.out.println("Vida del personaje: " + vida);
-        System.out.println("Cartas en el mazo:");
-
-        for (Carta carta : mazo) {
-            carta.imprimirInformacion();
-            System.out.println("-----");
-        }
     }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getVida() {
+        return vida;
+    }
+    public int getRonda() {
+        return ronda;
+    }
+    
 }
-
 
 
 
